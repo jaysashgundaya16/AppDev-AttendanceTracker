bf830ccffa45e9cb359a80b9cb3ce0eeffccc1b9
jaysashgundaya17@gmail.com <jaysashgundaya17@gmail.com>
2025-05-15 07:50:57 +0800

    Updates

--- assets/ios.transition-legacy-BUSEfQx0.js
+++ assets/ios.transition-legacy-BUSEfQx0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D0djREJX.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-UFSGeHpb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
