7f808c38c644853bb0a2e3523b42f3b99993c2f2
jaysashgundaya17@gmail.com <jamesivanfelicitas@gmail.com>
2025-05-15 07:13:28 +0800

    Updates

--- assets/ios.transition-legacy-DTIeRsff.js
+++ assets/ios.transition-legacy-DTIeRsff.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-0Cgdjlk1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BThr9JiL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
