d56a28ca93198ffc873e7de020622a3a5678ec98
jaysashgundaya17@gmail.com <jaysashgundaya17@gmail.com>
2025-05-15 07:34:27 +0800

    Updates

--- assets/md.transition-legacy-CNY64jCx.js
+++ assets/md.transition-legacy-CNY64jCx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BThr9JiL.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-D0djREJX.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
